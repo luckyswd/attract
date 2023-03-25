4fd083e8c1566fce25b96a87cf76a70875be9ff2
denis.shediakov <luckyswd@gmail.com>
2023-03-25 21:22:38 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-cd wp-app/wp-content/themes/attract/ && npm i && gulp build
+#cd wp-app/wp-content/themes/attract/ && npm i && gulp build
+git status
 git add .
 git commit -m "deploy build"
 git push
