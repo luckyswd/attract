f68fdd822423245eced4f748c1603d6f933716d7
denis.shediakov <luckyswd@gmail.com>
2023-03-25 20:49:33 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,1 @@
-#cd wp-app/wp-content/themes/attract/ && npm i && gulp build
-#rm -rf node_modules
-#rm -rf dist
-
-cd wp-app/wp-content/themes/attract/ && rm -rf dist
+cd wp-app/wp-content/themes/attract/ && npm i && gulp build
