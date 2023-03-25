3f657353e848fcb53187532bb11b323024a8a558
denis.shediakov <luckyswd@gmail.com>
2023-03-25 22:02:51 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd wp-app/wp-content/themes/attract/
 npm i && gulp build
-git config --global user.email "luckyswd@gmail.com"
-git config --global user.name "Denis Shediakov"
-git add .
-git commit -m "deploy build"
-git push
+#git config --global user.email "luckyswd@gmail.com"
+#git config --global user.name "Denis Shediakov"
+#git add .
+#git commit -m "deploy build"
+#git push
