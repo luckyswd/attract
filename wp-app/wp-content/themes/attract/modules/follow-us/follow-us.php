12d99b289108726e2bebbdb1d363634f527f76b1
sergey-geomotiv <sergey.k@geomotiv.com>
2023-03-26 12:53:51 +0300

    add follow-us

---
+++ wp-app/wp-content/themes/attract/modules/follow-us/follow-us.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+Title: Следи за нами
+Mode: preview
+*/
+
+?>
+
+<?php
+$headline = get_field('headline');
+$button = get_field('button');
+$side_image = get_field('side_image');
+?>
+
+<?php if (!is_admin()) : ?>
+    <section class="follow-us">
+        <div class="container">
+            <div class="follow-us__content">
+                <div class="follow-us__main">
+                    <?php if (!empty($headline)) : ?>
+                        <h2 class="follow-us__headline">
+                            <?= $headline ?>
+                        </h2>
+                    <?php endif; ?>
+
+                    <?php if (!empty($button)) : ?>
+                        <a href="<?= $button['url'] ?? '#' ?>" class="follow-us__button">
+                            <?= $button['title'] ?? '' ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+
+                <div class="follow-us__side">
+                    <?php if (!empty($side_image)) : ?>
+                        <div class="follow-us__image">
+                            <img loading="lazy" src="<?= $side_image['url'] ?>" alt="Картинка">
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php else: ?>
+    <h2 style="font-family: 'Mark', sans-serif;">Следи за нами</h2>
+<?php endif; ?>
