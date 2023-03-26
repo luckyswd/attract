97c457cb34d4ae733c4aa9ccf76c12745ebd56d1
sergey-geomotiv <sergey.k@geomotiv.com>
2023-03-26 15:17:05 +0300

    add feedback, promotion

---
+++ wp-app/wp-content/themes/attract/modules/feedback/feedback.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+Title: Отзывы
+Mode: preview
+*/
+
+?>
+
+<?php
+$headline = get_field('headline');
+$cards = get_field('cards');
+?>
+
+<?php if (!is_admin()) : ?>
+    <section class="feedback">
+        <div class="container">
+            <?php if (!empty($headline)) : ?>
+                <h2 class="feedback__headline">
+                    <?= $headline ?>
+                </h2>
+            <?php endif; ?>
+
+            <?php if (!empty($cards)) : ?>
+                <div class="feedback__cards">
+                    <?php foreach ($cards as $card) : ?>
+                        <div class="feedback__card">
+
+                            <?php if (!empty($card['client'])) : ?>
+                                <div class="feedback__client">
+                                    <?= $card['client'] ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <?php if (!empty($card['activity'])) : ?>
+                                <div class="feedback__activity">
+                                    <?= $card['activity'] ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <?php if (!empty($card['image'])) : ?>
+                                <div class="feedback__image">
+                                    <img loading="lazy" src="<?= $card['image']['url'] ?>" alt="Картинка">
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </section>
+<?php else: ?>
+    <h2 style="font-family: 'Mark', sans-serif;">Отзывы</h2>
+<?php endif; ?>
---
+++ wp-app/wp-content/themes/attract/modules/promotion/promotion.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Title: Продвижение
+Mode: preview
+*/
+
+?>
+
+<?php
+$headline = get_field('headline');
+$button = get_field('button');
+?>
+
+<?php if (!is_admin()) : ?>
+    <section class="promotion">
+        <div class="container">
+            <?php if (!empty($headline)) : ?>
+                <h2 class="promotion__headline">
+                    <?= $headline ?>
+                </h2>
+            <?php endif; ?>
+
+            <?php if (!empty($button)) : ?>
+                <a href="<?= $button['url'] ?>" class="promotion__button">
+                    <?= $button['title'] ?>
+                </a>
+            <?php endif; ?>
+        </div>
+
+
+    </section>
+<?php else: ?>
+    <h2 style="font-family: 'Mark', sans-serif;">Продвижение</h2>
+<?php endif; ?>
