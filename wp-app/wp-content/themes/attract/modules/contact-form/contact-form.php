516b26f2a21cda716ff76dbc24c3ac96417eb5e5
sergey-geomotiv <sergey.k@geomotiv.com>
2023-04-02 13:55:20 +0300

    added mobile contact form

--- wp-app/wp-content/themes/attract/modules/contact-form/contact-form.php
+++ wp-app/wp-content/themes/attract/modules/contact-form/contact-form.php
@@ -14,24 +14,27 @@ $form = get_field('form');
 ?>
 
 <?php if (!is_admin()) : ?>
-    <section class="contact-form" id="contact-form">
+    <section class="contact-form distance" id="contact-form">
         <div class="container">
-            <div class="contact-form__header">
-                <?php if (!empty($headline)) : ?>
-                    <h2 class="contact-form__headline">
-                        <?= $headline ?>
-                    </h2>
-                <?php endif; ?>
+            <div class="contact-form__wrapper">
+                <div class="contact-form__header">
+                    <?php if (!empty($headline)) : ?>
+                        <h2 class="contact-form__headline">
+                            <?= $headline ?>
+                        </h2>
+                    <?php endif; ?>
 
-                <?php if (!empty($icon)) : ?>
-                    <div class="contact-form__icon">
-                        <?= getPictureImage($icon) ?>
-                    </div>
+                    <?php if (!empty($icon)) : ?>
+                        <div class="contact-form__icon">
+                            <?= getPictureImage($icon) ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <?php if (!empty($form)) : ?>
+                    <?= $form ?>
                 <?php endif; ?>
             </div>
-            <?php if (!empty($form)) : ?>
-                <?= $form ?>
-            <?php endif; ?>
+
         </div>
     </section>
 <?php else: ?>
