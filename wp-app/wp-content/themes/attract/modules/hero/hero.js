10946d934c6cbdd2cd2c0fab346f67a9d065cf58
denis.shediakov <luckyswd@gmail.com>
2023-04-08 20:11:09 +0300

    fix

--- wp-app/wp-content/themes/attract/modules/hero/hero.js
+++ wp-app/wp-content/themes/attract/modules/hero/hero.js
@@ -12,7 +12,6 @@ class Hero {
       slidesPerView: 1,
       slidesPerGroup: 1,
       loopFillGroupWithBlank: true,
-      autoHeight: true,
       loop: true,
       autoplay: {
         delay: 4000,
