fbfa2b2114ff7a11d1c7cf015279aa9d31f7076e
denis.shediakov <luckyswd@gmail.com>
2023-04-08 16:24:06 +0300

    fix

--- wp-app/wp-content/themes/attract/modules/our-cases/our-cases.js
+++ wp-app/wp-content/themes/attract/modules/our-cases/our-cases.js
@@ -2,15 +2,13 @@ new Swiper(".our-cases__cards-swiper", {
     breakpoints: {
         320: {
             slidesPerView: 1,
-            spaceBetween: 5,
+            spaceBetween: 15,
             grabCursor: true,
             centeredSlides: true,
             loop: true,
             autoplay: {
                 delay: 2000,
             },
-
-
         },
         680: {
             slidesPerView: 2,
@@ -22,7 +20,6 @@ new Swiper(".our-cases__cards-swiper", {
                 delay: 2000,
             },
         },
-
 
         1441: {
             slidesPerView: 3,
