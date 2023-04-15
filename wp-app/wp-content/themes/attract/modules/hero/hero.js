82fb06af564e295b5186a37b90571978d07701f0
denis.shediakov <luckyswd@gmail.com>
2023-04-15 12:35:31 +0300

    fix

--- wp-app/wp-content/themes/attract/modules/hero/hero.js
+++ wp-app/wp-content/themes/attract/modules/hero/hero.js
@@ -11,12 +11,10 @@ class Hero {
     new Swiper(".swiper-hero", {
       slidesPerView: 1,
       slidesPerGroup: 1,
-      // loopFillGroupWithBlank: true,
-      // autoHeight: true,
-      // loop: true,
-      // autoplay: {
-      //   delay: 9000,
-      // },
+      loop: true,
+      autoplay: {
+        delay: 9000,
+      },
     });
   }
 }
