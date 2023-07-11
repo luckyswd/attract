187a97972fed3e7f0637b2e78576e8e469087139
denis.shediakov <luckyswd@gmail.com>
2023-07-11 23:36:09 +0300

    add steps module

---
+++ wp-app/wp-content/themes/attract/modules/steps/steps.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+Title: Шаги модуль
+Mode: preview
+*/
+
+?>
+
+<?php
+$headline = get_field('headline');
+$subheadline = get_field('subheadline');
+$cards = get_field('cards');
+?>
+
+<?php if (!is_admin()) : ?>
+    <section class="steps">
+        <div class="container">
+            <div class="steps-wrapper">
+                <div class="steps-left">
+                    <div class="steps-left-container">
+                        <p class="h4"> <?= $headline ?? '' ?> </p>
+                        <p class="text-2"><?= $subheadline ?? '' ?></p>
+                    </div>
+                </div>
+                <div class="steps-right">
+                    <?php if (!empty($cards)) : ?>
+                        <?php foreach ($cards as $key => $card) : ?>
+                        <div class="steps-card">
+                            <p class="card-number">(0<?= $key + 1 ?>)</p>
+                            <div class="card-bottom">
+                                <p class="h6"> <?=$card['headline'] ?> </p>
+                                <p class="text-4"> <?= $card['subheadline'] ?> </p>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php else: ?>
+    <h2 style="font-family: 'Mark', sans-serif;">Шаги модуль</h2>
+<?php endif; ?>
