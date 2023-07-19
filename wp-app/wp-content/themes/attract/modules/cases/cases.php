b181d8a76f1f3757f91ffe6bf45b6cf342987026
denis.shediakov <luckyswd@gmail.com>
2023-07-19 21:24:36 +0300

    fix

--- wp-app/wp-content/themes/attract/modules/cases/cases.php
+++ wp-app/wp-content/themes/attract/modules/cases/cases.php
@@ -19,7 +19,7 @@ $cases = get_field('cases');
         <div class="container">
             <div class="cases-top">
                 <p class="h2"><?= $headline ?? '' ?></p>
-                <a class="btn blue" href="<?= $button['link'] ?>"><?= $button['title'] ?? '' ?></a>
+                <a class="btn blue" href="<?= $button['url'] ?>"><?= $button['title'] ?? '' ?></a>
             </div>
             <?php if (!empty($cases)) : ?>
                 <div class="cases-wrapper">
