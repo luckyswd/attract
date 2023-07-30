671241b22226628b2d4da8a3c9f966ef74a9f4ac
denis.shediakov <luckyswd@gmail.com>
2023-07-30 17:09:34 +0300

    add case-text module

---
+++ wp-app/wp-content/themes/attract/modules/case-text/case-text.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+Title: Кейс текст модуль
+Mode: preview
+*/
+
+?>
+
+<?php
+$columns = get_field('columns');
+?>
+
+<?php if (!is_admin()) : ?>
+    <?php if (!empty($columns)) : ?>
+        <section class="case-text distance">
+            <div class="container">
+                <div class="case-text-wrap">
+                    <?php foreach ($columns as $column) : ?>
+                        <div class="case-text-column">
+                            <p class="text-1"> <?= $column['caption'] ?? '' ?> </p>
+                            <p class="h5"><?= $column['headline'] ?? '' ?></p>
+                            <p class="text-2"><?= $column['sub_headline'] ?? '' ?></p>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+    <?php endif; ?>
+<?php else: ?>
+    <h2 style="font-family: 'Mark', sans-serif;">Кейс текст модуль</h2>
+<?php endif; ?>
