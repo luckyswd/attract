0c76ea4d6aa6197151279a4cc3c6b07716630fa7
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2023-10-17 01:32:42 +0300

    feat: upgrade steps module

--- wp-app/wp-content/themes/attract/modules/steps/steps.php
+++ wp-app/wp-content/themes/attract/modules/steps/steps.php
@@ -8,10 +8,12 @@ Mode: preview
 ?>
 
 <?php
+$photos = get_field('employee_photos');
 $headline = get_field('headline');
 $subheadline = get_field('subheadline');
 $caption = get_field('caption');
 $cards = get_field('cards');
+$displayButton = get_field('display_button');
 ?>
 
 <?php if (!is_admin()) : ?>
@@ -21,6 +23,15 @@ $cards = get_field('cards');
             <div class="steps-wrapper">
                 <div class="steps-left">
                     <div class="steps-left-container">
+                        <?php if (!empty($photos)) : ?>
+                            <div class="steps-left-photos">
+                                <?php foreach ($photos as $photo): ?>
+                                    <div class="steps-left-photo">
+                                        <img src="<?= $photo['url']; ?>" alt="<?= $photo['alt']; ?>">
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
                         <?php if (!empty($caption)) : ?>
                             <p class="text-1"><?= $caption ?></p>
                         <?php endif; ?>
@@ -29,6 +40,9 @@ $cards = get_field('cards');
                         <?php endif; ?>
                         <?php if (!empty($subheadline)) : ?>
                             <p class="text-2"><?= $subheadline ?></p>
+                        <?php endif; ?>
+                        <?php if (!empty($displayButton)) : ?>
+                            <a href="#contact-form" class="btn blue"><span class="hover-animation"><span>Оставить заявку</span></span></a>
                         <?php endif; ?>
                     </div>
                 </div>
@@ -40,7 +54,7 @@ $cards = get_field('cards');
                                     <p class="card-number">(0<?= $key + 1 ?>)</p>
                                     <div class="card-bottom">
                                         <p class="h6"> <?= $card['headline'] ?> </p>
-                                        <p class="text-4"> <?= $card['subheadline'] ?> </p>
+                                        <div class="text-4"> <?= $card['subheadline'] ?> </div>
                                     </div>
                                 </div>
                             <?php endif; ?>
