daddac9ec4bda6a214eb88731ef392b6a074de5f
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2023-10-20 00:13:48 +0300

    refactor: update cases-for-services module

--- wp-app/wp-content/themes/attract/modules/cases-for-services/cases-for-services.php
+++ wp-app/wp-content/themes/attract/modules/cases-for-services/cases-for-services.php
@@ -18,15 +18,10 @@ $cases = get_field('cases');
         <div class="block-anchor" id="cases"></div>
         <div class="container">
             <div class="cases-top">
-                <p class="h4"><?= $headline ?? '' ?></p>
+                <p class="h3"><?= $headline ?? '' ?></p>
                 <a class="btn blue" href="<?= $button['url'] ?>">
                     <span class="hover-animation">
                         <span><?= $button['title'] ?? '' ?></span>
-                    </span>
-                </a>
-                <a class="btn blue" href="#contact-form">
-                    <span class="hover-animation">
-                        <span>Оставить заявку</span>
                     </span>
                 </a>
             </div>
