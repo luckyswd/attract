b54345b7e37c863f7ec398a3a302d25056ab6c85
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2023-10-22 17:02:48 +0300

    feat: create advantages module

---
+++ wp-app/wp-content/themes/attract/modules/advantages/advantages.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+Title: Преимущества модуль
+Mode: preview
+*/
+
+?>
+
+<?php
+$headline = get_field('headline');
+$advantages_cards = get_field('advantages_cards');
+?>
+
+<?php if (!is_admin()) : ?>
+    <section class="advantages distance">
+        <div class="container">
+            <div class="advantages-wrapper">
+                <div class="h3"><?= $headline ?? '' ?></div>
+                <?php if (!empty($advantages_cards)) : ?>
+                    <div class="advantages-cards">
+                        <?php foreach ($advantages_cards as $card) : ?>
+                            <?php if (!empty($card)) : ?>
+                                <div class="advantages-card"
+                                        style="background-image: url(<?= $card['background_image']['url'] ?? '' ?>)">
+                                    <p class="h5"> <?= $card['headline'] ?? '' ?> </p>
+                                    <p class="text-2"> <?= $card['subheadline'] ?? '' ?> </p>
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+<?php else: ?>
+    <h2 style="font-family: 'Mark', sans-serif;">Преимущества модуль</h2>
+<?php endif; ?>
