9cac85d564bf0c2301ed5a338d77c32139a059d6
artem <artemkomarec10@gmail.com>
2023-10-25 19:40:18 +0300

    feat: add divider to page title

--- wp-app/wp-content/themes/attract/header.php
+++ wp-app/wp-content/themes/attract/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title><?php wp_title('«', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <title><?php wp_title('«', true, 'right'); ?> | <?php bloginfo('name'); ?></title>
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php wp_head(); ?>
 </head>
