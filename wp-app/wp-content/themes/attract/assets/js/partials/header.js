5fc889bbf611cf5cecd2bde64c0ef6434a6ba602
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2023-10-27 14:33:05 +0300

    fix: update style for header

--- wp-app/wp-content/themes/attract/assets/js/partials/header.js
+++ wp-app/wp-content/themes/attract/assets/js/partials/header.js
@@ -1,6 +1,6 @@
 const header = document.querySelector('header')
 const burger = header.querySelector('.header__burger')
-const headerDesktopContainer = document.querySelector('.header__desktop')
+
 burger.addEventListener('click', () => {
     if (header.classList.contains('active-menu')) {
         header.classList.remove('active-menu')
@@ -15,17 +15,3 @@ headerButtonsMobile.forEach((btn) => {
         header.classList.remove('active-menu')
     })
 })
-
-function isTopPositionScroll() {
-    if (window.scrollY) {
-        headerDesktopContainer.classList.add('active');
-    } else {
-        headerDesktopContainer.classList.remove('active');
-    }
-}
-
-isTopPositionScroll()
-
-window.addEventListener('scroll', function() {
-    isTopPositionScroll()
-});
