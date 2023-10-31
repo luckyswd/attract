a7a18080ad3a82111fe3250752576f028ee10fe8
artem <artemkomarec10@gmail.com>
2023-10-31 11:21:41 +0300

    fix: team img block wrapper team block

--- wp-app/wp-content/themes/attract/components/picture-with-text.php
+++ wp-app/wp-content/themes/attract/components/picture-with-text.php
@@ -1,9 +1,9 @@
-<picture>
-<div class="team-img" style="background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0), rgb(0 0 0 / 73%)), url(<?= $image['url'] ?>);">
-</div>
+<div class="teammate-picture__wrap">
+    <div class="team-img" style="background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0), rgb(0 0 0 / 73%)), url(<?= $image['url'] ?>);">
+    </div>
     <div class="staff-position__info">
         <p class="staff-position__text"><?= $position; ?></p>
         <p class="h6"><?= $name; ?></p>
         <p class="text-3"><?= $description; ?></p>
     </div>
-</picture>
+</div>
