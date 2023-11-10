1f0285dd265414202c8a0f654d290730309e01a0
Slava Liutkevich <slava.lyutkevich8308@gmail.com>
2023-11-11 00:31:10 +0300

    refactor: update scripts for result module

--- wp-app/wp-content/themes/attract/modules/result/result.js
+++ wp-app/wp-content/themes/attract/modules/result/result.js
@@ -4,35 +4,38 @@ class Result {
     }
 
     init() {
-        const columns = parseInt(document.querySelector('.result-bottom').getAttribute('data-columns'));
-        const swiper = new Swiper(".result-bottom", {
-            loop: false,
-            spaceBetween: 20,
-
-            pagination: {
-                el: '.pagination',
-                clickable: true
-            },
-            navigation: false,
-
-            watchOverflow: true,
-
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 20,
-                    draggable: true
+        const resultBottom = document.querySelector('.result-bottom');
+        if (resultBottom) {
+            const columns = parseInt(resultBottom.getAttribute('data-columns'));
+            const swiper = new Swiper(".result-bottom", {
+                loop: false,
+                spaceBetween: 20,
+    
+                pagination: {
+                    el: '.pagination',
+                    clickable: true
                 },
-
-                769: {
-                    slidesPerView: columns - 1,
-                },
-
-                1024: {
-                    slidesPerView: columns,
+                navigation: false,
+    
+                watchOverflow: true,
+    
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                        draggable: true
+                    },
+    
+                    769: {
+                        slidesPerView: columns - 1,
+                    },
+    
+                    1024: {
+                        slidesPerView: columns,
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
 
