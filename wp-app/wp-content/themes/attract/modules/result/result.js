57431eddc02477d8091272d2a6fb5633e4262c2a
Denis <luckyswd@gmail.com>
2023-11-22 20:19:18 +0300

    fix result js

--- wp-app/wp-content/themes/attract/modules/result/result.js
+++ wp-app/wp-content/themes/attract/modules/result/result.js
@@ -1,43 +1,44 @@
 class Result {
-    constructor() {
-        this.init();
-    }
+  constructor() {
+    this.init();
+  }
 
-    init() {
-        const resultBottom = document.querySelector('.result-bottom');
-        if (resultBottom) {
-            const columns = parseInt(resultBottom.getAttribute('data-columns'));
+  init() {
+    const resultsBottom = document.querySelectorAll('.result-bottom');
 
-            new Swiper(".result-bottom", {
-                loop: false,
-                spaceBetween: 20,
-    
-                pagination: {
-                    el: '.pagination',
-                    clickable: true
-                },
-                navigation: false,
-    
-                watchOverflow: true,
-    
-                breakpoints: {
-                    320: {
-                        slidesPerView: 1,
-                        spaceBetween: 20,
-                        draggable: true
-                    },
-    
-                    769: {
-                        slidesPerView: columns - 1,
-                    },
-    
-                    1024: {
-                        slidesPerView: columns,
-                    }
-                }
-            });
+    resultsBottom && resultsBottom.forEach((swiperContainer) => {
+      const columns = parseInt(swiperContainer.getAttribute('data-columns'));
+
+      new Swiper(swiperContainer, {
+        loop: false,
+        spaceBetween: 20,
+
+        pagination: {
+          el: '.pagination',
+          clickable: true
+        },
+        navigation: false,
+
+        watchOverflow: true,
+
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            draggable: true
+          },
+
+          769: {
+            slidesPerView: columns - 1,
+          },
+
+          1024: {
+            slidesPerView: columns,
+          }
         }
-    }
+      });
+    })
+  }
 }
 
 new Result();
