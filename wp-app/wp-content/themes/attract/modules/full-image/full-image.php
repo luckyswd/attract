5364c34a2f05e26b108e7c312bc2cca95acd5dc4
artem <artemkomarec10@gmail.com>
2024-02-05 13:11:52 +0300

    feat: add headline to full image section

--- wp-app/wp-content/themes/attract/modules/full-image/full-image.php
+++ wp-app/wp-content/themes/attract/modules/full-image/full-image.php
@@ -9,10 +9,12 @@ Mode: preview
 
 <?php
 $image = get_field('image');
+$headline = get_field('headline');
 ?>
 
 <?php if (!is_admin()) : ?>
     <section class="full-image distance">
+        <p class="h2"><?= $headline; ?></p>
         <img src="<?= $image['url'] ?>" alt="<?= $image['alt']; ?>">
     </section>
 <?php else: ?>
