544cbec1cc94735bd222c6c4203f53eb5b6ab1a7
Alex Sonets <xamel1ion@Noutbuk-Alex.local>
2024-02-09 22:38:19 +0300

    fix: blog post content fix styles

--- wp-app/wp-content/themes/attract/modules/single-post-content/single-post-content.php
+++ wp-app/wp-content/themes/attract/modules/single-post-content/single-post-content.php
@@ -20,26 +20,51 @@ $blocks = get_field("content-blocks");
         <?php endif; ?>
         <div class="content-block__row">
             <?php foreach($block['block'] as $col): ?>
-                <?php $col = $col['column'] ?>
                 <div class="content-block__cl <?php echo !empty($col['full-width']) && !!$col['full-width'] ? 'content-block__cl_full-width' : '' ?>">
-                    <?php if(!empty($col['blue-title'])): ?>
-                        <div class="content-block__blue-title text-1"><?= $col['blue-title']; ?></div>
-                    <?php endif; ?>
-                    <?php if(!empty($col['title'])): ?>
-                        <div class="content-block__title h5"><?= $col['title']; ?></div>
-                    <?php endif; ?>
-                    <?php if(!empty($col['sub-title'])): ?>
-                        <div class="content-block__sub-title text-1"><?= $col['sub-title']; ?></div>
-                    <?php endif; ?>
-                    <?php if(!empty($col['number'])): ?>
-                        <div class="content-block__number"><?= $col['number']; ?></div>
-                    <?php endif; ?>
-                    <?php if(!empty($col['text'])): ?>
-                        <div class="content-block__text text-2"><?= $col['text']; ?></div>
-                    <?php endif; ?>
-                    <?php if(!empty($col['blue-box-text'])): ?>
-                        <div class="content-block__blue-box-text text-1"><?= $col['blue-box-text']; ?></div>
-                    <?php endif; ?>
+                    <?php $col = $col['column'] ?>
+                    <?php //var_dump($col); ?>
+                    <?php foreach($col as $el): ?>
+                        <?php
+                        switch ($el['acf_fc_layout']):
+                            case 'blue-title':
+                                ?>
+                                <div class="content-block__blue-title text-1"><?= $el['text']; ?></div>
+                                <?php
+                                break;
+                            case 'title':
+                                ?>
+                                <div class="content-block__title h5"><?= $el['text']; ?></div>
+                                <?php
+                                break;
+                            case 'sub-title':
+                                ?>
+                                <div class="content-block__sub-title text-1"><?= $el['text']; ?></div>
+                                <?php
+                                break;
+                            case 'blue-box-text':
+                                ?>
+                                <div class="content-block__blue-box-text text-1"><?= $el['text']; ?></div>
+                                <?php
+                                break;
+                            case 'text-block-wrapper':
+                                ?>
+                                <div class="content-block__texts-wrapper">
+                                    <?php $i = 1; ?>
+                                    <?php foreach($el['text-block'] as $text_block): ?>
+                                        <div class="content-block__text-wrapper">
+                                            <?php if(!empty($el['show-numbers']) && !!$el['show-numbers']): ?>
+                                                <div class="content-block__number">(<?php echo str_pad($i, 2, "0", STR_PAD_LEFT) ?>)</div>
+                                            <?php endif; ?>
+                                            <div class="content-block__text text-2"><?= $text_block['text']; ?></div>
+                                        </div>
+                                        <?php $i++; ?>
+                                    <?php endforeach; ?>
+                                </div>
+                                <?php
+                                break;
+                        endswitch;
+                        ?>
+                    <?php endforeach; ?>
                 </div>
             <?php endforeach; ?>
         </div>
