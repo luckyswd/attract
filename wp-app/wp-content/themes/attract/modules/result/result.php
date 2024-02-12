b4958b34f7cb1a3b32ed12c1ba4bf86279f68e20
artem <artemkomarec10@gmail.com>
2024-02-12 21:11:12 +0300

    fix: revert changes

--- wp-app/wp-content/themes/attract/modules/result/result.php
+++ wp-app/wp-content/themes/attract/modules/result/result.php
@@ -16,13 +16,12 @@ $caption = get_field('caption');
 $headline = get_field('headline');
 $text = get_field('text');
 $result_blocks = get_field('result_blocks');
-$full_width_img = get_field("horizontal_image");
 ?>
 
 <?php if (!is_admin()) : ?>
     <section class="result">
         <div class="container">
-            <div class="result-wrapper" <?php if ($imagesCount <= 0) : ?> style="gap: 0" <?php endif; ?>>
+            <div class="result-wrapper" <?php if ($imagesCount <= 0) : ?> style="gap: 0" <?php endif;?>>
                 <div class="result-top">
                     <?php if (!empty($caption)) : ?>
                         <p class="text-1"><?= $caption ?></p>
@@ -47,24 +46,16 @@ $full_width_img = get_field("horizontal_image");
                         </div>
                     <?php endif; ?>
                 </div>
-                <div class="result-bottom__wrap">
-                    <?php foreach ($full_width_img as $image) : ?>
-                        <div class="full-width__img">
-                            <img src="<?= $image['url'] ?>" alt="<?= $image['alt']; ?>">
-                        </div>
-                    <?php endforeach; ?>
-                    <div class="swiper result-bottom" data-columns="<?= $columnsCount ?>">
-                        <div class="swiper-wrapper">
-                            <?php foreach ($images as $image) : ?>
-                                <div class="swiper-slide result-picture <?= $objectFit ?>">
-                                    <img src="<?= $image['url'] ?>" alt="<?= $image['alt']; ?>">
-                                </div>
-                            <?php endforeach; ?>
-                        </div>
-                        <div class="pagination"></div>
+                <div class="swiper result-bottom" data-columns="<?= $columnsCount ?>">
+                    <div class="swiper-wrapper">
+                        <?php foreach($images as $image) : ?>
+                            <div class="swiper-slide result-picture <?= $objectFit ?>">
+                                <img src="<?= $image['url'] ?>" alt="<?= $image['alt']; ?>">
+                            </div>
+                        <?php endforeach; ?>
                     </div>
+                    <div class="pagination"></div>
                 </div>
-
             </div>
         </div>
     </section>
