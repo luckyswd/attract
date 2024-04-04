1a5874e1e4f37c495aa3e008b48644862f1442ca
IsWEBtime <moneyfeve@gmail.com>
2024-04-04 12:53:42 +0300

    fix: add title for section-service-cats module

--- wp-app/wp-content/themes/attract/components/section-service-cats.php
+++ wp-app/wp-content/themes/attract/components/section-service-cats.php
@@ -4,15 +4,21 @@ $cats = get_terms( 'service-category', [
 	'hide_empty' => true,
     'fields'     => 'id=>name'
 ] );
+$title = get_field('title')
 ?>
 <section class="service-tax-categories distance">
     <div class="container">
-       <div class="service-tax-categories__cards">
-            <?php foreach($cats as $id => $name): ?>
-            <a href="<?= get_term_link($id, 'service-category') ?>"  class="service-tax-categories__card <?= $tax->term_id === $id ? 'active' : ''; ?> text-3">
-                <span><?= $name ?></span>
-            </a>
-            <?php endforeach; ?>
-       </div>
+        <?php if(!empty($title)): ?>
+            <div class="section__header">
+                <h2 class="h3 section__title"><?= $title ?></h2>
+            </div>
+        <?php endif; ?>
+        <div class="service-tax-categories__cards">
+                <?php foreach($cats as $id => $name): ?>
+                <a href="<?= get_term_link($id, 'service-category') ?>"  class="service-tax-categories__card <?= $tax->term_id === $id ? 'active' : ''; ?> text-3">
+                    <span><?= $name ?></span>
+                </a>
+                <?php endforeach; ?>
+        </div>
     </div>
 </section>
