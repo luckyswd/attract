2a980b39e2d89b2bbdb7421fa1570f6348dba20b
IsWEBtime <moneyfeve@gmail.com>
2024-04-16 22:45:11 +0300

    fix: change logic to mark right and bottom cards

--- wp-app/wp-content/themes/attract/modules/our-clients/our-clients.js
+++ wp-app/wp-content/themes/attract/modules/our-clients/our-clients.js
@@ -1,17 +1,19 @@
 class OurClients {
-    grid = document.querySelector('[data-grid]');
-    gridSize = {
-        w: 0,
-        h: 0
-    }
     constructor() {
 
-        const cards = document.querySelectorAll('[data-grid-card]');
-        for(let card of cards) {
+        this.grid = document.querySelector('[data-grid]');
+        this.gridSize = {
+            w: 0,
+            h: 0
+        }
+
+        this.cards = document.querySelectorAll('[data-grid-card]');
+        for(let card of this.cards) {
             this.init(card);
         }
 
         this.calcGridSize();
+        this.markGridItems();
 
         document.addEventListener('click', (e) => {
             if(e.target.dataset.gridCardModalClose !== undefined) {
@@ -21,6 +23,7 @@ class OurClients {
 
         window.addEventListener('resize', (e) => {
             this.calcGridSize();
+            this.markGridItems();
         })
     }
 
@@ -29,10 +32,26 @@ class OurClients {
         this.gridSize.h = this.grid.offsetHeight;
     }
 
+    markGridItems() {
+        for(let card of this.cards) {
+            if((this.gridSize.h - card.offsetTop - card.offsetHeight) < card.offsetHeight){
+                card.classList.add('bottom');
+            } else {
+                card.classList.remove('bottom');
+            }
+
+            if((this.gridSize.w - card.offsetLeft - card.offsetWidth) < card.offsetWidth){
+                card.classList.add('right');
+            } else {
+                card.classList.remove('right');
+            }
+        }
+    }
+
     resetAllActiveCard() {
         const cards = document.querySelectorAll('[data-grid-card].active');
         for(let card of cards) {
-            card.classList.remove('active', 'bottom', 'right')
+            card.classList.remove('active')
         }
     }
 
@@ -42,14 +61,14 @@ class OurClients {
             
             this.resetAllActiveCard();
             card.classList.add('active');
-            console.log((this.gridSize.h - card.offsetTop - card.offsetHeight), card.offsetHeight);
-            if((this.gridSize.h - card.offsetTop - card.offsetHeight) < card.offsetHeight){
-                card.classList.add('bottom');
-            }
+            // console.log((this.gridSize.h - card.offsetTop - card.offsetHeight), card.offsetHeight);
+            // if((this.gridSize.h - card.offsetTop - card.offsetHeight) < card.offsetHeight){
+            //     card.classList.add('bottom');
+            // }
 
-            if((this.gridSize.w - card.offsetLeft - card.offsetWidth) < card.offsetWidth){
-                card.classList.add('right');
-            }
+            // if((this.gridSize.w - card.offsetLeft - card.offsetWidth) < card.offsetWidth){
+            //     card.classList.add('right');
+            // }
             
         })
     }
