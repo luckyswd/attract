047405613f2753091c58789fadd1dc44821c4eb7
IsWEBtime <moneyfeve@gmail.com>
2024-06-26 20:49:05 +0300

    refactor: optimize myths module

--- wp-app/wp-content/themes/attract/modules/myths/myths.js
+++ wp-app/wp-content/themes/attract/modules/myths/myths.js
@@ -1,17 +1,26 @@
-const questions = document.querySelectorAll('.myths-question');
+(() => {
+    const init = () => {
+        const questions = document.querySelectorAll('.myths-question');
 
-questions.forEach(function(question) {
-    const mythsQuestionTop = question.querySelector('.myths-question-top');
+        questions.forEach(function(question) {
+            const mythsQuestionTop = question.querySelector('.myths-question-top');
 
-    mythsQuestionTop.addEventListener('click', function() {
-        const mythsQuestion = question;
+            mythsQuestionTop.addEventListener('click', function() {
+                const mythsQuestion = question;
 
-        questions.forEach(function(q) {
-            if (q !== mythsQuestion) {
-                q.classList.remove('js-active');
-            }
+                questions.forEach(function(q) {
+                    if (q !== mythsQuestion) {
+                        q.classList.remove('js-active');
+                    }
+                });
+
+                mythsQuestion.classList.toggle('js-active');
+            });
         });
+    }
 
-        mythsQuestion.classList.toggle('js-active');
+    document.addEventListener('DOMContentLoaded', () => {
+        !!document.querySelector('.myths-questions') && init();
     });
-});
+
+})();
