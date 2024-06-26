7cb50cb1ce1641b81002c9f07d7a89c6d018d2d0
IsWEBtime <moneyfeve@gmail.com>
2024-06-26 21:26:45 +0300

    refactor: optimize footer  modules

--- wp-app/wp-content/themes/attract/assets/js/partials/footer.js
+++ wp-app/wp-content/themes/attract/assets/js/partials/footer.js
@@ -1,9 +1,11 @@
-const categories = document.querySelectorAll('.category-wrap');
+(() => {
+  const categories = document.querySelectorAll('.category-wrap');
 
-categories && categories.forEach((category) => {
-  const cat = category.querySelector('.service_category');
+  categories && categories.forEach((category) => {
+    const cat = category.querySelector('.service_category');
 
-  cat.addEventListener('click', () => {
-    category.classList.toggle('js-active');
+    cat.addEventListener('click', () => {
+      category.classList.toggle('js-active');
+    })
   })
-})
+})();
